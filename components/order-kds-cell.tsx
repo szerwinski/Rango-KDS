e9189346b4762bcce60de8a146fa219f28e476de
lucasmma <lukito020@gmail.com>
2025-03-16 13:12:43 -0300

    feat: add some more informations on orderKdsCell

--- components/order-kds-cell.tsx
+++ components/order-kds-cell.tsx
@@ -1,10 +1,18 @@
 import { FB } from './base'
-import { P2 } from './typography'
+import { P1, P2 } from './typography'
 
 export default function OrderKdsCell() {
-  return (<FB fd='column' className='w-full p-2 rounded-xl'>
-    <FB className='w-full p-2 rounded-md bg-primary'>
 
+
+  return (<FB fd='column' className='w-full p-2 rounded-xl'>
+    <FB ha='start' className='w-full gap-3 p-2 rounded-md bg-primary'>
+      <FB className="relative">
+        <div className="w-10 h-10 border-2 border-t-0 border-[white] border-solid rounded-full animate-spin" />
+        <P1 className='text-[white] absolute !text-[12px]'>42m</P1>
+      </FB>
+      <P1 className='text-[white]'>Pedido #1231</P1>
+      <P1 className='text-[white] !font-light'>|</P1>
+      <P1 className='text-[white] !font-light'>Mesa 34</P1>
     </FB>
     <FB fd='column' ha='start' className='w-full p-2 gap-2 rounded-md bg-[#2a2f41]'>
       {
