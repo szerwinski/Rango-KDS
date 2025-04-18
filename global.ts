0098f14d1f34526a77e5d1bff26c12341a5c13ba
Lucas Mendonça <lukito020@gmail.com>
2025-04-18 11:42:26 -0300

    feat: create apollo config variables

--- global.ts
+++ global.ts
@@ -1,1 +1,32 @@
+import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
 export const API_URL = "http://localhost:1337/v2/";
+const HASURA_URL_HOMOLOG = "whole-buck-36.hasura.app/v1/graphql";
+
+const httpLink = new HttpLink({ uri: "http://" + HASURA_URL_HOMOLOG });
+
+const wsLink =
+  typeof window !== "undefined"
+    ? new GraphQLWsLink(createClient({ url: "ws://" + HASURA_URL_HOMOLOG }))
+    : null;
+
+const link = wsLink
+  ? split(
+      ({ query }) => {
+        const def = getMainDefinition(query);
+        return (
+          def.kind === "OperationDefinition" && def.operation === "subscription"
+        );
+      },
+      wsLink,
+      httpLink,
+    )
+  : httpLink;
+
+export const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+});
