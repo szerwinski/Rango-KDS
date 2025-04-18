34e9d805188e53168d6d61b6aa320bdfe852837c
Lucas Mendonça <lukito020@gmail.com>
2025-04-18 11:43:29 -0300

    feat: use hook for user and graphQL

--- pages/kds.tsx
+++ pages/kds.tsx
@@ -1,37 +1,65 @@
-import { FB, FlexButton, SD, SectionWrapper, Spacer } from '../components/base'
-import { H1, H2, P1, P2 } from '../components/typography'
-import Image from 'next/image'
-import useWindowInfo from '../hooks/useWindowInfo'
-import { useState } from 'react'
-import classNames from 'classnames'
-import { Restaurant } from '../model'
-import axios, { Canceler } from 'axios'
-import Button from '../components/buttons/button'
-import Drawer from '../components/drawer'
-import Header from '../components/header'
-import OrderKdsCell from '../components/order-kds-cell'
+import { FB, FlexButton, SD, SectionWrapper, Spacer } from "../components/base";
+import { H1, H2, P1, P2 } from "../components/typography";
+import Image from "next/image";
+import useWindowInfo from "../hooks/useWindowInfo";
+import { useState } from "react";
+import classNames from "classnames";
+import { Restaurant } from "../model";
+import axios, { Canceler } from "axios";
+import Button from "../components/buttons/button";
+import Drawer from "../components/drawer";
+import Header from "../components/header";
+import OrderKdsCell from "../components/order-kds-cell";
+import { useTableSalesSubscription } from "../hooks/useSalesGraphQL";
+import useUser from "../hooks/useUser";
+import Loading from "../components/loading";
 
 export default function Kds() {
+  const user = useUser();
+  const { tableSales, loading, error } = useTableSalesSubscription(
+    user?.restaurant.id ?? 0,
+    new Date().toISOString(),
+  );
+
   return (
     <>
-      <SectionWrapper hasScreenWidth hasScreenHeight className='items-center justify-center bg-background'>
-        <FB fd='row' h='h-full' va='start' className='w-full'>
+      <SectionWrapper
+        hasScreenWidth
+        hasScreenHeight
+        className="items-center justify-center bg-background"
+      >
+        <FB fd="row" h="h-full" va="start" className="w-full">
           <Drawer />
-          <FB fd='column' va='start' className='w-full min-h-[100vh] ml-[80px] bg-background'>
+          <FB
+            fd="column"
+            va="start"
+            className="ml-[80px] min-h-[100vh] w-full bg-background"
+          >
             <Header />
-            <FB fd='column' va='start' className='w-[calc(100%-40px)] p-8 rounded-xl h-full my-4 bg-background2'>
-              <FB className='gap-2 mb-4'>
-                <H2 className='text-[white]'>KDS </H2>
-                <H2 className='text-[white] !font-normal'> (Kitchen Display System)</H2>
+            {loading ? (
+              <Loading />
+            ) : (
+              <FB
+                fd="column"
+                va="start"
+                className="my-4 h-full w-[calc(100%-40px)] rounded-xl bg-background2 p-8"
+              >
+                <FB className="mb-4 gap-2">
+                  <H2 className="text-[white]">KDS </H2>
+                  <H2 className="!font-normal text-[white]">
+                    {" "}
+                    (Kitchen Display System)
+                  </H2>
+                </FB>
+                <div className="grid w-full grid-cols-4 gap-4">
+                  <OrderKdsCell />
+                  <OrderKdsCell />
+                </div>
               </FB>
-              <div className="grid w-full grid-cols-4 gap-4">
-                <OrderKdsCell />
-                <OrderKdsCell />
-              </div>
-            </FB>
+            )}
           </FB>
         </FB>
       </SectionWrapper>
     </>
-  )
+  );
 }
