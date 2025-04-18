9e3877bae045892e632dcf4ad57bd5a7aaf25e3c
Lucas Mendonça <lukito020@gmail.com>
2025-04-18 11:46:38 -0300

    feat: pass id, data and nomeMesa for orderKdsCell

--- components/order-kds-cell.tsx
+++ components/order-kds-cell.tsx
@@ -1,36 +1,48 @@
-import { FB } from './base'
-import { P1, P2 } from './typography'
+import { OrderItem } from "../model";
+import { FB } from "./base";
+import { P1, P2 } from "./typography";
 
-export default function OrderKdsCell() {
-
-
-  return (<FB fd='column' className='w-full p-2 rounded-xl'>
-    <FB ha='start' className='w-full gap-3 p-2 rounded-md bg-primary'>
-      <FB className="relative">
-        <div className="w-10 h-10 border-2 border-t-0 border-[white] border-solid rounded-full animate-spin" />
-        <P1 className='text-[white] absolute !text-[12px]'>42m</P1>
+export default function OrderKdsCell({
+  data,
+  id,
+  nomeMesa,
+}: {
+  data: OrderItem[];
+  id: number;
+  nomeMesa: string;
+}) {
+  return (
+    <FB fd="column" className="w-full rounded-xl p-2">
+      <FB ha="start" className="w-full gap-3 rounded-md bg-primary p-2">
+        <FB className="relative">
+          <div className="h-10 w-10 animate-spin rounded-full border-2 border-t-0 border-solid border-[white]" />
+          <P1 className="absolute !text-[12px] text-[white]">42m</P1>
+        </FB>
+        <P1 className="text-[white]">Pedido #{id}</P1>
+        <P1 className="!font-light text-[white]">|</P1>
+        <P1 className="!font-light text-[white]">Mesa {nomeMesa}</P1>
       </FB>
-      <P1 className='text-[white]'>Pedido #1231</P1>
-      <P1 className='text-[white] !font-light'>|</P1>
-      <P1 className='text-[white] !font-light'>Mesa 34</P1>
-    </FB>
-    <FB fd='column' ha='start' className='w-full p-2 gap-2 rounded-md bg-[#2a2f41]'>
-      {
-        [1, 2, 3].map((item, index) => {
+      <FB
+        fd="column"
+        ha="start"
+        className="w-full gap-2 rounded-md bg-[#2a2f41] p-2"
+      >
+        {data.map((item, index) => {
           return (
-            <P2 className='text-[white] px-2 py-1'>1 x Pedido {item}</P2>
-          )
-        })
-      }
-      <FB className='w-full gap-2'>
-        <FB className='w-1/2 p-2 rounded-md bg-[red]'>
-          <P2 className='text-[white]'>Ocultar</P2>
-        </FB>
-        <FB className='w-1/2 p-2 rounded-md bg-[green]'>
-          <P2 className='text-[white]'>Ocultar</P2>
+            <P2 className="px-2 py-1 text-[white]">
+              {item.quantity} x Pedido {item.menu_item.name}
+            </P2>
+          );
+        })}
+        <FB className="w-full gap-2">
+          <FB className="w-1/2 rounded-md bg-[red] p-2">
+            <P2 className="text-[white]">Ocultar</P2>
+          </FB>
+          <FB className="w-1/2 rounded-md bg-[green] p-2">
+            <P2 className="text-[white]">Ocultar</P2>
+          </FB>
         </FB>
       </FB>
     </FB>
-  </FB>
-  )
+  );
 }
