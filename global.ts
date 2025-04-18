5dc16b982cd4f52da079c79fe203b29c107bee8c
Lucas Mendonça <lukito020@gmail.com>
2025-04-18 18:10:47 -0300

    feat: add hasurasecret

--- global.ts
+++ global.ts
@@ -5,12 +5,28 @@ import { getMainDefinition } from "@apollo/client/utilities";
 
 export const API_URL = "http://localhost:1337/v2/";
 const HASURA_URL_HOMOLOG = "whole-buck-36.hasura.app/v1/graphql";
+const HASURA_SECRET =
+  "e4NUc6FuJBXFrkGmaG3sB310qpY13ds1nbas9gLh1BHsncx6sGzswOFvgny0pe5a";
 
-const httpLink = new HttpLink({ uri: "http://" + HASURA_URL_HOMOLOG });
+const httpLink = new HttpLink({
+  uri: "https://" + HASURA_URL_HOMOLOG,
+  headers: {
+    "x-hasura-admin-secret": HASURA_SECRET,
+  },
+});
 
 const wsLink =
   typeof window !== "undefined"
-    ? new GraphQLWsLink(createClient({ url: "ws://" + HASURA_URL_HOMOLOG }))
+    ? new GraphQLWsLink(
+        createClient({
+          url: "wss://" + HASURA_URL_HOMOLOG,
+          connectionParams: {
+            headers: {
+              "x-hasura-admin-secret": HASURA_SECRET,
+            },
+          },
+        }),
+      )
     : null;
 
 const link = wsLink
