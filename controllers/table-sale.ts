f3f5735b0e987f0d892d499a6ab818ef59b3b850
Lucas Mendonça <lukito020@gmail.com>
2025-04-18 11:53:29 -0300

    feat: add some filters and populate it

--- controllers/table-sale.ts
+++ controllers/table-sale.ts
@@ -4,7 +4,11 @@ import { TableSale } from "../model";
 
 export default class TableSaleController {
   static async getTableSaleById(id: number): Promise<TableSale | null> {
-    const URL = API_URL + "table/sales?filters[id][eq]=" + id;
+    const URL =
+      API_URL +
+      "table/sales?filters[id][$eq]=" +
+      id +
+      "&populate[0]=table&populate[1]=cashier&populate[2]=payment&populate[3]=payment.paymentList&populate[4]=data&populate[5]=data.options&populate[6]=data.menu_item";
     const tableSale = await axios.get(URL);
 
     if (tableSale.data) {
